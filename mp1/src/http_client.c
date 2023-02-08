1044a76a79856d856e906023d3b9467efb786af4
unknown <jiayuz6@illinois.edu>
2023-02-08 01:36:38 -0600

    untested client

--- mp1/src/http_client.c
+++ mp1/src/http_client.c
@@ -17,7 +17,11 @@
 #define PORT "3490" // the port client will be connecting to 
 
 #define MAXDATASIZE 100 // max number of bytes we can get at once 
-
+#define GET_HEADER "GET %s HTTP/1.1\r\n\r\n"
+// GET /test.txt HTTP/1.1
+// User-Agent: Wget/1.12 (linux-gnu)
+// Host: localhost:3490
+// Connection: Keep-Alive
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
@@ -44,7 +48,7 @@ void parse_input(char * input, char * ip, char * path, char * port) {
 		if (*p == ':') { // if there is a port, before : is ip address 
 			strncpy(ip, s, p - s); // record ip address in to buffer
 			port_found = 1;
-			port_start = p;
+			port_start = p + 1;
 		}
 		if (*p == '/') {
 			if (port_found == 1) { // if there is a port we scan over, we are at the / immediately after port, thus, 
@@ -64,43 +68,51 @@ void parse_input(char * input, char * ip, char * path, char * port) {
 	while (*p != 0) {
 		p++;
 	}
-	if (port_found == 0) {
-		*port = 80;
-	}
 	strncpy(path, path_start, p - path_start);
 }
 
 
 int main(int argc, char *argv[])
 {
-	int sockfd, numbytes;  
+	int sockfd;  
 	char buf[MAXDATASIZE];
 	struct addrinfo hints, *servinfo, *p;
 	int rv;
 	char s[INET6_ADDRSTRLEN];
 
-	char path[2048];
-	char ip[2048];
-	char port[8];
-	memset(path, 0, sizeof(path));
-	memset(ip, 0, sizeof(ip));
-	memset(port, 0, sizeof(port));
-	parse_input(argv[1], ip, path, port);
-	//printf("\n %s, %s, %s", path, ip, port);
+
 	if (argc != 2) {
 	    fprintf(stderr,"usage: client hostname\n");
 	    exit(1);
 	}
 
+	char path[512];
+	char ip[512];
+	char port[8];
+	memset(path, 0, sizeof(path));
+	memset(ip, 0, sizeof(ip));
+	memset(port, 0, sizeof(port));
+	parse_input(argv[1], ip, path, port);
+	if (*port == 0) {
+		strcpy(port, "80");
+	}
+	printf("\n %s, %s, %s", path, ip ,port);
+	char request[2048];
+	memset(request, 0, sizeof(request));
+	sprintf(request, GET_HEADER, path);
+	int length = strlen(request);
+	printf("%s", request);
+
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
-	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+	/* ________________________________________________________________*/
+	// connect to the server
+	if ((rv = getaddrinfo(ip, port, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
-
 	// loop through all the results and connect to the first we can
 	for(p = servinfo; p != NULL; p = p->ai_next) {
 		if ((sockfd = socket(p->ai_family, p->ai_socktype,
@@ -126,18 +138,50 @@ int main(int argc, char *argv[])
 	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
 			s, sizeof s);
 	printf("client: connecting to %s\n", s);
-
 	freeaddrinfo(servinfo); // all done with this structure
+	/*_________________________________________________________________________*/
+	// end of connecting
+	// start sending the header to the server
+	int total_sent = 0;
 
-	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-	    perror("recv");
-	    exit(1);
+	// send the header to the server
+	while(total_sent < length) {
+		int sent = send(sockfd, request, length, 0);
+		if (sent == -1) {
+			perror("sending:failure");
+		}
+		total_sent = total_sent + sent;
 	}
 
-	buf[numbytes] = '\0';
+	// now all message sent to server
+	// hanging for server to send its header and the requested file
+	// write the output file to buf
+	while(1) {
+		int receive_bytes = recv(sockfd, buf, MAXDATASIZE, 0);
+		if (receive_bytes == -1) {
+			perror("receive_file:");
+			return 1;
+		}
+		if (receive_bytes == 0) {
+			break;
+		}
+	}
+	// now all the file is stored inside buf, I can start parsing the output
+	FILE * output = fopen("output", "wb");
+	int file_len = strlen(buf);
+	fwrite(buf, 1, file_len, output);
 
-	printf("client: received '%s'\n",buf);
 
+
+	// if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+	//     perror("recv");
+	//     exit(1);
+	// }
+
+	// buf[numbytes] = '\0';
+
+	// printf("client: received '%s'\n",buf);
+	fclose(output);
 	close(sockfd);
 
 	return 0;
